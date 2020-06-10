2bec1aa8733a92551d4cf69c91cacdfe7d1bdd21
Mauricio Manfro <6717975+mmanfro@users.noreply.github.com>
2020-06-10 10:36:53 -0300

    media url

--- djinn/urls.py
+++ djinn/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
 from django.conf import settings
 
 
@@ -8,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', include('inc_mgmt.urls', namespace='inc_mgmt')),
     path('chat/', include('chat.urls', namespace='chat')),
-] + static(settings.MEDIA_URL)
+    path(settings.MEDIA_URL)
+]
